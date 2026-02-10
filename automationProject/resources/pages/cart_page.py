b7750473e757bba7d635fac35de41c9b65fe5aeb
Mathijesij <mathijesi.johnbritto@kumaran.com>
2026-02-10 14:16:19 +0530

    Delete automationProject directory

--- automationProject/resources/pages/cart_page.py
+++
@@ -1,7 +0,0 @@
-class CartPage:
-    def __init__(self, page, locator):
-        self.page = page
-        self.locator = locator
-
-    def proceed_to_checkout(self):
-        self.page.locator(self.locator["proceed_to_checkout"]).click()
